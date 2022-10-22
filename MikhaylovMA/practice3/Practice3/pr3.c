3f737510b3385557b124b0bc90f7c255088a4254
maxim-haruspexxx-mikhaylov <maximmikhaylov00@gmail.com>
2022-10-22 18:12:49 +0300

    final

--- MikhaylovMA/practice3/Practice3/pr3.c
+++ MikhaylovMA/practice3/Practice3/pr3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define n 10
 int main() {
 	short nums[n];
-	short length;
-	char check;
+	short length, bulls, cows, try_count = 0, cur_num;
+	int guess;
+	char check, check2;
 	srand((unsigned int)time(NULL));
 
 	do {
@@ -32,5 +34,51 @@ int main() {
 
 	for (short i = 0; i < length; i++) {
 		printf("%hd", nums[i]);
+	}
+	printf("\n");
+
+	printf("I chose the number. Try to guess it! (type '-1' for answer)\n");
+	while (1) {
+		check2 = '0';
+		try_count++;
+		bulls = 0;
+		cows = 0;
+
+		do {
+			printf("What's your guess? \n");
+			scanf("%d", &guess);
+		} while (!((guess >= pow(10, length - 1) && guess <= pow(10, length) - 1) || guess == -1));
+
+		if (guess == -1) {
+			for (short i = 0; i < length; i++) {
+				printf("%hd", nums[i]);
+			}
+			printf("\n");
+			try_count--;
+		}
+		else {
+			for (short i = length - 1; i >= 0; i--) {
+				cur_num = guess % 10;
+				if (cur_num == nums[i]) bulls++;
+				else {
+					check2 = '0';
+					for (short i = 0; i < length; i++) {
+						if (cur_num == nums[i]) {
+							check2 = '1';
+							break;
+						}
+					}
+					if (check2 == '1') cows++;
+				}
+				guess /= 10;
+			}
+			if (bulls == length) {
+				printf("You're right, mooooo! You've guessed the number in %hd tries. \n", try_count);
+				break;
+			}
+			else {
+				printf("No. Bulls - %hd. Cows - %hd. (Tries - %hd) \n", bulls, cows, try_count);
+			}
+		}
 	}
 }
