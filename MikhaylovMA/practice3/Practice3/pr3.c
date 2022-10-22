12f9dff1575c996c10d5ea1c0c3ce3e9d9d1df65
maxim-haruspexxx-mikhaylov <maximmikhaylov00@gmail.com>
2022-10-22 17:04:04 +0300

    working_num_rand

--- MikhaylovMA/practice3/Practice3/pr3.c
+++ MikhaylovMA/practice3/Practice3/pr3.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define n 5
+#define n 10
 int main() {
 	short nums[n];
 	short length;
+	char check;
 	srand((unsigned int)time(NULL));
 
 	do {
-		printf("Insert number length (2-5)");
+		printf("Insert number length (2-5) \n");
 		scanf("%hd", &length);
 	} while (length < 2 || length > 5);
 
-	for (short i = 0; i < n - 1; i++) {
-		for 
+	for (short i = 0; i < length; i++) {
+		do {
+			nums[i] = rand() % 10;
+			check = '0';
+			if (nums[0] == 0) check = '1';
+			else {
+				for (short j = 0; j < i; j++) {
+					if (nums[j] == nums[i]) {
+						check = '1';
+						break;
+					}
+				}
+			}
+		} while (check != '0');
+	}
+
+	for (short i = 0; i < length; i++) {
+		printf("%hd", nums[i]);
 	}
 }
