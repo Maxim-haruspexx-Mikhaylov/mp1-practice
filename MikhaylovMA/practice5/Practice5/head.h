636a3f92e548f4b370293eae9a51793ea1b37110
maxim-haruspexxx-mikhaylov <maximmikhaylov00@gmail.com>
2022-12-21 00:53:43 +0300

    init_commit

---
+++ MikhaylovMA/practice5/Practice5/head.h
@@ -0,0 +1,6 @@
+#pragma once
+void get_path();
+void get_method();
+void sort_1();
+void sort_2();
+void sort_3();
---
+++ MikhaylovMA/practice5/Practice5/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <windows.h>
+#include "head.h"
+#include "func.c"
+
+void main() {
+
+}
